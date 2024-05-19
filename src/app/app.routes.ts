bb9ec7041260f1e34e6ac1fe6c48056d6cfafd25
RominaNavarrete <rominagnavarrete@gmail.com>
2024-05-19 19:38:13 -0300

    interceptors

---
+++ src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { PokemonListComponent } from './pages/pokemon-list/pokemon-list.component';
+
+export const routes: Routes = [{ path: '', component: PokemonListComponent }];
